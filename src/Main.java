d07a160e16dcdb3f92a8ec817eae39c44e4cc0e0
GustavoLyra23 <gustavolyra23@gmail.com>
2024-05-17 16:05:56 -0300

    position added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+     Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
---
+++ src/Position.java
@@ -0,0 +1,35 @@
+public class Position {
+
+    private int row;
+    private int col;
+
+    public Position(int row, int col) {
+        super();
+        this.row = row;
+        this.col = col;
+    }
+
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + col;
+    }
+
+
+}
